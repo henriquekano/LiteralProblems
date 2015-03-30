0903bb413cccbb3a51b516addfc57849a4eed2e0
henriquekano <henriquekano@hotmail.com>
2015-03-30 16:53:05 -0300

    stuck @ dealing with exponencials

--- LiteralString.rb
+++ LiteralString.rb
@@ -1,11 +1,48 @@
-#restricoes:  expoentes reais
+#restricoes:  expoentes reais positivos
 #             componentes no formato (N)(var)
 #             inexistencia de parenteses
+#             parcelas "atomicas"
 
 class LiteralString < String
 
   def initialize(string)
-    @string = string
+    @hash = {}
+
+    string.sub(/-/, "+-").split("+").each_with_index do |element, i|
+      parts = divide_number_variables(element)
+      @hash[parts[1]] = Float(parts[0])
+    end
+
+  end
+
+  def to_s
+    string = ""
+
+    @hash.each_with_index do |(key, value), index|
+      unless index == 0 or value < 0
+        string += "+"
+      end
+
+      unless value == 0
+        if value == 1
+          string += key.to_s
+        else
+          string += value.to_s + key.to_s
+        end
+      end
+    end
+
+    if string == ""
+      string = "0"
+    end
+
+
+
+    string
+  end
+
+  def hash
+    @hash
   end
 
   def +(something)
@@ -13,88 +50,20 @@ class LiteralString < String
       something = something.to_s
     end
 
-    new_part_not_included = true
-    result = ""
-    result_array = []
-    @string.split("+|-").each_with_index do |element, i|
+    parts = divide_number_variables(something)
+    numeric_part = parts[0]
+    alpha_part = parts[1]
 
-      parts = divide_number_variables(element)
-      numeric_part = parts[0]
-      alpha_part = parts[1]
-
-      if numeric_part == nil
-        numeric_part = ""
-      end
-
-      if alpha_part == nil
-        alpha_part = ""
-      end
-
-      if something == alpha_part
-        if numeric_part == ""
-          numeric_part = "1"
-        end
-        numeric_part = (Float(numeric_part) + 1.0).to_s
-        new_part_not_included = false
-      elsif is_numeric(something) and alpha_part == ""
-        numeric_part = (Float(numeric_part) + Float(something)).to_s
-        new_part_not_included = false
-      end
-
-      result_array[i] = numeric_part + alpha_part
+    if @hash[alpha_part] != nil
+      @hash[alpha_part] += Float(numeric_part)
+    else
+      @hash[alpha_part] = Float(numeric_part)
     end
 
-    result = result_array.join("+")
-
-    if new_part_not_included
-      result = result + "+" + something
-    end
-
-    result
+    self
   end
 
   def *(something)
-    unless something.is_a?(String)
-      something = something.to_s
-    end
-
-    result = ""
-    result_array = []
-    @string.split("+").each_with_index do |element, i|
-
-      parts = divide_number_variables(element)
-      numeric_part = parts[0]
-      alpha_part = parts[1]
-
-      if numeric_part == nil
-        numeric_part = ""
-      end
-
-      if alpha_part == nil
-        alpha_part = ""
-      end
-
-      if something == alpha_part
-        if numeric_part == ""
-          numeric_part = "1"
-        end
-        numeric_part = (Float(numeric_part) + 1.0).to_s
-        new_part_not_included = false
-      elsif is_numeric(something) and alpha_part == ""
-        numeric_part = (Float(numeric_part) + Float(something)).to_s
-        new_part_not_included = false
-      end
-
-      result_array[i] = numeric_part + alpha_part
-    end
-
-    result = result_array.join("+")
-
-    if new_part_not_included
-      result = result + "+" + something
-    end
-
-    result
   end
 
   def /(something)
@@ -102,30 +71,56 @@ class LiteralString < String
   end
 
   def -(something)
-    
+    unless something.is_a?(String)
+      something = something.to_s
+    end
+
+    parts = divide_number_variables(something)
+    numeric_part = parts[0]
+    alpha_part = parts[1]
+
+    if @hash[alpha_part] != nil
+      @hash[alpha_part] -= Float(numeric_part)
+    else
+      @hash[alpha_part] = Float(numeric_part)
+    end
+
+    self
   end
 
   private
   def is_numeric(something)
-    true if Float(self) rescue false  
+    true if Float(something) rescue false  
   end
 
   def divide_number_variables(string)
-    result = []
-    parts = string.split(/([a-zA-Z\^]+[\d]+)|/) - [""]#.reject! {|string| string.empty?}# [a-zA-Z]+(\^\d+)?|
-    if parts.length == 1
-      if is_numeric(parts[0])
-        result = [parts[0], ""]
-      else
-        result = ["", parts[0]]
-      end
-    elsif parts.length == 2
-      result = parts
+    parts = []
+    numeric_part = string.match(/((?!\^)(-)?\d+(\.\d+)?)/)#.reject! {|string| string.empty?}# [a-zA-Z]+(\^\d+)?|
+    variable_part = string.match(/[a-zA-Z]+(\^)?\d*/)
+
+    if numeric_part
+      parts << numeric_part[0]
     else
-      p "Shit Happened"
+      parts << nil
     end
 
-    result
+    if variable_part
+      parts << variable_part[0]
+    else
+      parts << nil
+    end
+
+    if parts[0] == nil
+      parts[0] = "1"
+    elsif parts[1] == nil
+      parts[1] = ""
+    end
+    p parts
+    parts
   end
 
 end
+
+
+test = LiteralString.new("a^2")+"a"
+p test.to_s
--- tests.rb
+++ tests.rb
@@ -3,15 +3,27 @@ require "minitest/autorun"
 
 class TestLiteralString < Minitest::Test
 
+  def test_general
+    assert_equal("0", (LiteralString.new("a")-"a").to_s)
+  end
+
   def test_plus
-    assert_equal("2.0a", LiteralString.new("a")+"a")
-    assert_equal("3.0a", LiteralString.new("2a")+"a")
-    assert_equal("b+a", LiteralString.new("b")+"a")
-    assert_equal("ab+a", LiteralString.new("ab")+"a")
-    assert_equal("a^2+a", LiteralString.new("a^2")+"a")
-    assert_equal("a^2b^2+a", LiteralString.new("a^2b^2")+"a")
-    assert_equal("abc^2+abc", LiteralString.new("abc^2")+"abc")
-    assert_equal("2.0abc^2", LiteralString.new("abc^2")+"abc^2")
+    assert_equal("2.0a", (LiteralString.new("a")+"a").to_s)
+    assert_equal("3.0a", (LiteralString.new("2.0a")+"a").to_s)
+    assert_equal("3.0a", (LiteralString.new("2a")+"a").to_s)
+    assert_equal("b+a", (LiteralString.new("b")+"a").to_s)
+    assert_equal("ab+a", (LiteralString.new("ab")+"a").to_s)
+    assert_equal("a^2+a", (LiteralString.new("a^2")+"a").to_s)
+    assert_equal("a^2b^2+a", (LiteralString.new("a^2b^2")+"a").to_s)
+    assert_equal("abc^2+abc", (LiteralString.new("abc^2")+"abc").to_s)
+    assert_equal("2.0abc^2", (LiteralString.new("abc^2")+"abc^2").to_s)
+    assert_equal("a", (LiteralString.new("a")+"0").to_s)
+    assert_equal("a-b", (LiteralString.new("a-b")+"0").to_s)
+  end
+
+  def test_minus
+    assert_equal(LiteralString.new("0").to_s, (LiteralString.new("a")-"a").to_s)
+    assert_equal("-1.0a", (LiteralString.new("a")-"2a").to_s)
   end
 
 end
